010987e78efc0fc5377764e5ac619176c6951a67
Cemetery7 <berendenis@yahoo.com>
2025-04-26 19:03:24 +0300

    init

--- cmd/url-shertener/main.go
+++ cmd/url-shertener/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
 	// TODO: init config: cleanenv
 
 	// TODO: init logger: slog
